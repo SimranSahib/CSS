d2d500da263d07a6f429ffe535a8de2f778aa2c1
SimranSahib <95210853+SimranSahib@users.noreply.github.com>
2021-12-12 18:45:13 -0700

    CSS
    This is CSS Cheetsheet

---
+++ Css.js
@@ -0,0 +1,257 @@
+const oncoming16 = document.querySelector('#o');
+
+oncoming16.addEventListener('mouseenter', () => {
+  if (!oncoming16.classList.contains('hover')) {
+    oncoming16.classList.add('hover');
+  }
+});
+
+oncoming16.addEventListener('mouseleave', () => {
+  if (oncoming16.classList.contains('hover')) {
+    oncoming16.classList.remove('hover');
+  }
+});
+
+
+const oncoming17 = document.querySelector('#x');
+
+oncoming17.addEventListener('mouseenter', () => {
+  if (!oncoming17.classList.contains('hover')) {
+    oncoming17.classList.add('hover');
+  }
+});
+
+oncoming17.addEventListener('mouseleave', () => {
+  if (oncoming17.classList.contains('hover')) {
+    oncoming17.classList.remove('hover');
+  }
+});
+
+
+
+const oncoming = document.querySelector('#a');
+
+oncoming.addEventListener('mouseenter', () => {
+  if (!oncoming.classList.contains('hover')) {
+    oncoming.classList.add('hover');
+  }
+});
+
+oncoming.addEventListener('mouseleave', () => {
+  if (oncoming.classList.contains('hover')) {
+    oncoming.classList.remove('hover');
+  }
+});
+
+
+
+const oncoming2 = document.querySelector('#b');
+
+oncoming2.addEventListener('mouseenter', () => {
+  if (!oncoming2.classList.contains('hover')) {
+    oncoming2.classList.add('hover');
+  }
+});
+
+oncoming2.addEventListener('mouseleave', () => {
+  if (oncoming2.classList.contains('hover')) {
+    oncoming2.classList.remove('hover');
+  }
+});
+
+
+
+const oncoming3 = document.querySelector('#c');
+
+oncoming3.addEventListener('mouseenter', () => {
+  if (!oncoming3.classList.contains('hover')) {
+    oncoming3.classList.add('hover');
+  }
+});
+
+oncoming3.addEventListener('mouseleave', () => {
+  if (oncoming3.classList.contains('hover')) {
+    oncoming3.classList.remove('hover');
+  }
+});
+
+
+
+const oncoming4 = document.querySelector('#d');
+
+oncoming4.addEventListener('mouseenter', () => {
+  if (!oncoming4.classList.contains('hover')) {
+    oncoming4.classList.add('hover');
+  }
+});
+
+oncoming4.addEventListener('mouseleave', () => {
+  if (oncoming4.classList.contains('hover')) {
+    oncoming4.classList.remove('hover');
+  }
+});
+
+
+
+
+const oncoming5 = document.querySelector('#e');
+
+oncoming5.addEventListener('mouseenter', () => {
+  if (!oncoming5.classList.contains('hover')) {
+    oncoming5.classList.add('hover');
+  }
+});
+
+oncoming5.addEventListener('mouseleave', () => {
+  if (oncoming5.classList.contains('hover')) {
+    oncoming5.classList.remove('hover');
+  }
+});
+
+
+
+
+const oncoming6 = document.querySelector('#f');
+
+oncoming6.addEventListener('mouseenter', () => {
+  if (!oncoming6.classList.contains('hover')) {
+    oncoming6.classList.add('hover');
+  }
+});
+
+oncoming6.addEventListener('mouseleave', () => {
+  if (oncoming6.classList.contains('hover')) {
+    oncoming6.classList.remove('hover');
+  }
+});
+
+
+
+
+const oncoming7 = document.querySelector('#g');
+
+oncoming7.addEventListener('mouseenter', () => {
+  if (!oncoming7.classList.contains('hover')) {
+    oncoming7.classList.add('hover');
+  }
+});
+
+oncoming7.addEventListener('mouseleave', () => {
+  if (oncoming7.classList.contains('hover')) {
+    oncoming7.classList.remove('hover');
+  }
+});
+
+
+
+
+const oncoming8 = document.querySelector('#h');
+
+oncoming8.addEventListener('mouseenter', () => {
+  if (!oncoming8.classList.contains('hover')) {
+    oncoming8.classList.add('hover');
+  }
+});
+
+oncoming8.addEventListener('mouseleave', () => {
+  if (oncoming8.classList.contains('hover')) {
+    oncoming8.classList.remove('hover');
+  }
+});
+
+
+
+
+const oncoming9 = document.querySelector('#i');
+
+oncoming9.addEventListener('mouseenter', () => {
+  if (!oncoming9.classList.contains('hover')) {
+    oncoming9.classList.add('hover');
+  }
+});
+
+oncoming9.addEventListener('mouseleave', () => {
+  if (oncoming9.classList.contains('hover')) {
+    oncoming9.classList.remove('hover');
+  }
+});
+
+
+
+const oncoming0 = document.querySelector('#j');
+
+oncoming0.addEventListener('mouseenter', () => {
+  if (!oncoming0.classList.contains('hover')) {
+    oncoming0.classList.add('hover');
+  }
+});
+
+oncoming0.addEventListener('mouseleave', () => {
+  if (oncoming0.classList.contains('hover')) {
+    oncoming0.classList.remove('hover');
+  }
+});
+
+
+
+const oncoming11 = document.querySelector('#k');
+
+oncoming11.addEventListener('mouseenter', () => {
+  if (!oncoming11.classList.contains('hover')) {
+    oncoming11.classList.add('hover');
+  }
+});
+
+oncoming11.addEventListener('mouseleave', () => {
+  if (oncoming11.classList.contains('hover')) {
+    oncoming11.classList.remove('hover');
+  }
+});
+
+
+const oncoming12 = document.querySelector('#l');
+
+oncoming12.addEventListener('mouseenter', () => {
+  if (!oncoming12.classList.contains('hover')) {
+    oncoming12.classList.add('hover');
+  }
+});
+
+oncoming12.addEventListener('mouseleave', () => {
+  if (oncoming12.classList.contains('hover')) {
+    oncoming12.classList.remove('hover');
+  }
+});
+
+
+
+const oncoming14 = document.querySelector('#m');
+
+oncoming14.addEventListener('mouseenter', () => {
+  if (!oncoming14.classList.contains('hover')) {
+    oncoming14.classList.add('hover');
+  }
+});
+
+oncoming14.addEventListener('mouseleave', () => {
+  if (oncoming14.classList.contains('hover')) {
+    oncoming14.classList.remove('hover');
+  }
+});
+
+
+
+
+const oncoming15= document.querySelector('#n');
+
+oncoming15.addEventListener('mouseenter', () => {
+  if (!oncoming15.classList.contains('hover')) {
+    oncoming15.classList.add('hover');
+  }
+});
+
+oncoming15.addEventListener('mouseleave', () => {
+  if (oncoming15.classList.contains('hover')) {
+    oncoming15.classList.remove('hover');
+  }
+});
